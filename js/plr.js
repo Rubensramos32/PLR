d001ea95133c7480aa711cce9ecc880b23f05ad2
Rubensramos32 <rubensramos955@gmail.com>
2024-09-13 19:59:10 -0300

    Update plr.js
    alteração de PLR.

--- js/plr.js
+++ js/plr.js
@@ -2,7 +2,7 @@ document.getElementById("plrForm").addEventListener("submit", function(event) {
     event.preventDefault();
     var nota = parseFloat(document.getElementById("nota").value.replace(',', '.'));//ignora ponto e virgula do usuario
     var salario = parseFloat(document.getElementById("salario").value.replace(/\./g, '').replace(',', '.'));// ignora ponto e virgula do usuario
-    var plr = 0.800;
+    var plr = 0.700;
     var meses_trabalhados = parseInt(document.getElementById("meses_trabalhados").value);
     var calculo = ((((nota * salario) * plr) / 12) * meses_trabalhados) / 100; // Dividindo por 100 para ficar no formato milhares
     var calculo_formatado = calculo.toFixed(2);
